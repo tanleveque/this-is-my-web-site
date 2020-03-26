69b63ff49a7e3edeca363f19093d65bfacd80db4
tanneguy levêque <tanleveque@gmail.com>
2020-03-26 15:07:44 +0100

    add card

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import './Main.css';
 import profilpicture from './image/profil-picture.jpg';
+import iotschema from './image/IoT-schema.png';
+import cultureeasy from './image/cultureEasy.png';
+import blockchainvsquantumcomputing from './image/bvsqc.jpg';
+import {
+  Nav, NavItem,
+  Card, CardTitle, CardImg, CardImgOverlay
+} from 'reactstrap';
 
 class App extends Component {
   render() {
@@ -8,30 +15,59 @@ class App extends Component {
       <div>
 
         <div className="profil-banner"></div>
-
-        <div className="image-contour">
-          <div className="image-container">
-            <img className="profil-picture" src={profilpicture} alt="me" />
+        <div className="profil-zone">
+          <div className="image-contour">
+            <div className="image-container">
+              <img className="profil-picture" src={profilpicture} alt="me" />
+            </div>
           </div>
-        </div>
 
-        <div>
-          <p className="myName">Tanneguy Levêque</p>
+          <div>
+            <p className="myName">Tanneguy Levêque</p>
             <div className="details">
               <p className="description">Student in Enginering and UX</p>
               <p className="localisation">Paris, France</p>
             </div>
+          </div>
         </div>
 
-        <div>
-          <ul>
-            <li className="list">Info</li>
-            <li className="list">Work</li>
-          </ul>
+        <div className="profil-content ">
+          <Nav className="justify-content-center">
+
+            <NavItem className="myNavItem font-weight-bold">Info</NavItem>
+            <NavItem className="myNavItem font-weight-bold">Work</NavItem>
+          </Nav>
+
+          <div className="myProject">
+            <div className="coverCard">
+              <Card inverse>
+                <CardImg src={iotschema} alt="Iot schema" />
+                <CardImgOverlay className="cardOverlay">
+                  <CardTitle className="card-title">Internet of Things : la technologie Zolertia</CardTitle>
+                </CardImgOverlay>
+              </Card>
+            </div>
+
+            <div className="coverCard">
+              <Card inverse>
+                <CardImg  src={cultureeasy} alt="culture easy" />
+                <CardImgOverlay className="cardOverlay">
+                  <CardTitle className="card-title">Projet étudiant : Culture Easy</CardTitle>
+                </CardImgOverlay>
+              </Card>
+            </div>
+
+            <div className="coverCard">
+              <Card inverse>
+                <CardImg  src={blockchainvsquantumcomputing} alt="Blockchain vs Quantum Computing" />
+                <CardImgOverlay className="cardOverlay">
+                  <CardTitle className="card-title">Blockchain vs Quantum Computing</CardTitle>
+                </CardImgOverlay>
+              </Card>
+            </div>
+          </div>
+
         </div>
-
-
-
       </div>
     );
   }
